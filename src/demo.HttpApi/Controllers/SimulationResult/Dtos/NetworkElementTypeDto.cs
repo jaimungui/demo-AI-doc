9bbdad4517fdf34d539c8909d760930b6a5cddb5
jaime.muniz <jaime.muniz@se.com>
2025-10-02 16:40:20 +0200

    sdf

---
+++ src/demo.HttpApi/Controllers/SimulationResult/Dtos/NetworkElementTypeDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Simulation.SimulationHub.SimulationResult.Dtos;
+
+public class NetworkElementTypeDto
+{
+    public string ObjectType { get; set; }
+    public string OriginalObjectType { get; set; }
+    public bool HasLength { get; set; }
+    public List<ConfigurationParameterDataDto> Definitions { get; set; } = new List<ConfigurationParameterDataDto>();
+}
