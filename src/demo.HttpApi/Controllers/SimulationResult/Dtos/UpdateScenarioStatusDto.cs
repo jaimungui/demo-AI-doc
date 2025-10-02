2a235dc3a8e519ba043fd0e53cd2b107f98e63f7
jaime.muniz <jaime.muniz@se.com>
2025-10-02 16:42:30 +0200

    asd

--- src/demo.HttpApi/Controllers/SimulationResult/Dtos/UpdateScenarioStatusDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Simulation.SimulationHub.Constants;
-using System;
-
-namespace Simulation.SimulationHub.SimulationResult.Dtos;
-
-public class UpdateScenarioStatusDto
-{
-    public RegistrationStatus Status { get; set; }
-}
