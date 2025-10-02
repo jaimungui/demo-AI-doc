c33de9e21dfd64ffd8b2eb035b81283c2c8621db
jaime.muniz <jaime.muniz@se.com>
2025-10-02 16:27:03 +0200

    sdf

--- src/demo.HttpApi/Controllers/Attribute/Dtos/GetAttributeValuesRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Simulation.SimulationHub.Attribute.Dtos
-{
-    public record GetAttributeValuesRequest(Guid ScenarioId, Guid NetworkElementId, DateTime StartDate, DateTime EndDate,
-        string Category, List<string> AttributeNames);
-}
