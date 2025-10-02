897eb4cf2964c01aa9ad9bb5cae2b985f294ce12
jaime.muniz <jaime.muniz@se.com>
2025-10-02 15:55:15 +0200

    ssdf

---
+++ src/demo.HttpApi/Controllers/Attribute/Dtos/AttributeDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Simulation.SimulationHub.Attribute.Dtos
+{
+    public class AttributeDto
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public DateTimeOffset ResultDateTime { get; set; }
+    }
+}
---
+++ src/demo.HttpApi/Controllers/Attribute/Dtos/DateRangeDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simulation.SimulationHub.Attribute.Dtos
+{
+    public class DateRangeDto
+    {
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset? EndDate { get; set; }
+        public List<DateTimeOffset> Steps { get; set; }
+
+        public DateRangeDto()
+        {
+            Steps = new List<DateTimeOffset>();
+        }
+    }
+}
