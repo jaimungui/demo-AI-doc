b88909f77c09bc6b85adb85c2d64ed4b277b0ff7
jaime.muniz <jaime.muniz@se.com>
2025-10-02 16:02:17 +0200

    sdf

---
+++ src/demo.HttpApi/Controllers/Attribute/Dtos/ElementTypeCategoriesDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.SimulationHub.Attribute.Dtos
+{
+    public class ElementTypeCategoriesDto
+    {
+        public string Type { get; set; }
+        public List<AttributeMetadataDto> Categories { get; set; }
+    }
+}
