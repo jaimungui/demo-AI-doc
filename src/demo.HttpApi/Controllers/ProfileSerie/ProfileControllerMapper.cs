3287eb77bb4fdcf90d7da250d2aa5d7edbd3c624
jaime.muniz <jaime.muniz@se.com>
2025-10-03 08:29:09 +0200

    sdf

---
+++ src/demo.HttpApi/Controllers/ProfileSerie/ProfileControllerMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Simulation.SimulationHub.ProfileSerie.Dtos;
+using Simulation.SimulationHub.ProfileSerie.GetProfileSeries;
+using Simulation.SimulationHub.ProfileSerie.GetScenarioProfiles;
+
+namespace Simulation.SimulationHub.ScenarioProfile
+{
+    public class ProfileControllerMapper : Profile
+    {
+        public ProfileControllerMapper()
+        {
+            CreateMap<ProfileCmdResponse, ScenarioProfileDto>();
+            CreateMap<GetScenarioProfilesCmdResponse, GetScenarioProfilesDto>();
+
+            CreateMap<ProfileValueCmdResponse, ProfileValueDto>();
+            CreateMap<ProfileSerieCmdResponse, ProfileSerieDto>();
+            CreateMap<GetProfileSeriesCmdResponse, GetProfileSeriesDto>();
+        }
+    }
+}
