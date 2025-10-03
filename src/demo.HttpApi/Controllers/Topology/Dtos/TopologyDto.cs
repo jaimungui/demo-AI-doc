3209e977a768c2770bfa31ecb0db69c3f0f37013
jaime.muniz <jaime.muniz@se.com>
2025-10-03 09:56:28 +0200

    sdsd

---
+++ src/demo.HttpApi/Controllers/Topology/Dtos/TopologyDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simulation.SimulationHub.Topology;
+
+public class TopologyDto
+{
+    public Guid ScenarioId { get; set; }
+    public Dictionary<string, ObjectTopologyDto> Objects { get; set; } = new Dictionary<string, ObjectTopologyDto>();
+    public List<PathDto> Paths { get; set; } = new List<PathDto>();
+    public List<ProfilePathDto> Profiles { get; set; } = new List<ProfilePathDto>();
+    public List<ProfileSettingsDto> ProfileSettings { get; set; } = new List<ProfileSettingsDto>();
+}
