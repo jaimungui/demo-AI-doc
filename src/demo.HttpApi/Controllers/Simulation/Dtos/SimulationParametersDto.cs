7df8eba89c769dba02604bb8a14aea62bfc1d0d2
jaime.muniz <jaime.muniz@se.com>
2025-10-03 10:31:50 +0200

    asd

---
+++ src/demo.HttpApi/Controllers/Simulation/Dtos/SimulationParametersDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simulation.SimulationHub.Simulation.Dtos
+{
+
+    public class SimulationParametersDto : BaseSimulationParametersDto
+    {
+        public DateTimeOffset Date { get; set; }
+        public TimeSpan StartDate { get; set; }
+        public TimeSpan StopDate { get; set; }
+    }
+}
