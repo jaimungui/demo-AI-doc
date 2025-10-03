d12c079eaaf983afcf98879af3c0d8a9127f660a
jaime.muniz <jaime.muniz@se.com>
2025-10-03 08:18:24 +0200

    sadas

---
+++ src/demo.HttpApi/Controllers/Simulation/Dtos/CyclicSimulationParametersDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Simulation.SimulationHub.Simulation.Dtos
+{
+
+    public class CyclicSimulationParametersDto : BaseSimulationParametersDto
+    {
+        public TimeSpan Cycle { get; set; }
+        public TimeSpan Period { get; set; }
+    }
+}
