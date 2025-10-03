c57c41434ef3b07c8ab2f6eca52573a2bcf4553c
jaime.muniz <jaime.muniz@se.com>
2025-10-03 11:12:07 +0200

    asda

---
+++ src/demo.HttpApi/Controllers/Topology/Dtos/NetworkElementResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simulation.SimulationHub.Topology
+{
+    public class NetworkElementResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double[] Centroid { get; set; }
+    }
+}
---
+++ src/demo.HttpApi/Controllers/Topology/Dtos/ProfileSettingsDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Simulation.SimulationHub.Topology;
+
+public class ProfileSettingsDto
+{
+    public string Id { get; set; }
+    public List<string> Attributes { get; set; }
+}
