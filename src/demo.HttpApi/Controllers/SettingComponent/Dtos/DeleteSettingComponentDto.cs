19409809f9aea0e6f290be9b04d014388d92ccad
jaime.muniz <jaime.muniz@se.com>
2025-10-03 11:17:34 +0200

    test

---
+++ src/demo.HttpApi/Controllers/SettingComponent/Dtos/DeleteSettingComponentDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Simulation.SimulationHub.SettingComponent.Dtos;
+
+public class DeleteSettingComponentDto
+{
+    [Required]
+    public List<SettingComponentDto> SettingsToDelete { get; set; }
+}
+
+public class SettingComponentDto
+{
+    public string SettingArea { get; set; }
+    public string SettingKey { get; set; }
+}
---
+++ src/demo.HttpApi/Controllers/SettingComponent/Dtos/UpsertSettingComponentResponseDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Simulation.SimulationHub.SettingComponent.Dtos;
+
+public class UpsertSettingComponentResponseDto
+{
+    public List<string> Success { get; set; }
+    public List<string> SuccessUsers { get; set; }
+}
