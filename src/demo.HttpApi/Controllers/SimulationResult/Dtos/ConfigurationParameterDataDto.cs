e8db7ab267902f35288bd0db367031bf7985ba49
jaime.muniz <jaime.muniz@se.com>
2025-10-03 09:01:52 +0200

    asd

---
+++ src/demo.HttpApi/Controllers/SimulationResult/Dtos/ConfigurationParameterDataDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Simulation.SimulationHub.SimulationResult.Dtos;
+
+public class ConfigurationParameterDataDto
+{
+    public string Name { get; set; }
+    public string Alias { get; set; }
+    public string Group { get; set; }
+    public string Type { get; set; }
+    public PhysTypeDto PhysType { get; set; }
+    public bool IsWeb { get; set; }
+
+}
---
+++ src/demo.HttpApi/Controllers/SimulationResult/Dtos/ScenarioSettingsDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simulation.SimulationHub.SimulationResult.Dtos;
+
+public class ScenarioSettingsDto
+{
+    public Guid ExecutionId { get; set; }
+    public DateTimeOffset FromDate { get; set; }
+    public DateTimeOffset ToDate { get; set; }
+    public List<NetworkElementTypeDto> Types { get; set; }
+}
