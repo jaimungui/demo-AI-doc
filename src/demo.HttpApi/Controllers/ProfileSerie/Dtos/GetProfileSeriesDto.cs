44e855dcce2c1820f101a9c463c51453124b2d7a
jaime.muniz <jaime.muniz@se.com>
2025-10-03 14:13:28 +0200

    sdf

---
+++ src/demo.HttpApi/Controllers/ProfileSerie/Dtos/GetProfileSeriesDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Simulation.SimulationHub.ProfileSerie.Dtos
+{
+    public class GetProfileSeriesDto
+    {
+        public string Lengend { get; set; }
+        public string ChainageUnits { get; set; }
+        public string ChainagePhysType { get; set; }
+        public IEnumerable<ProfileSerieDto> ProfileSerieValues { get; set; }
+
+        public GetProfileSeriesDto()
+        {
+            ProfileSerieValues = new List<ProfileSerieDto>();
+        }
+    }
+}
---
+++ src/demo.HttpApi/Controllers/ProfileSerie/Dtos/GetScenarioProfilesDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simulation.SimulationHub.ProfileSerie.Dtos;
+
+public class GetScenarioProfilesDto
+{
+    public Dictionary<Guid,List<ScenarioProfileDto>> ScenarioProfiles { get; set; }
+
+    public GetScenarioProfilesDto()
+    {
+        ScenarioProfiles = new Dictionary<Guid, List<ScenarioProfileDto>>();
+    }
+}
